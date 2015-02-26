71f598f4ce50b88110ad3ef97c281a2f6ddf5eff
jcl <joe@lemura.com.au>
2015-02-26 18:17:58 +1100

    Initial Commit

---
+++ Profile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Profile
+//
+//  Created by Joe Lemura on 26/02/2015.
+//  Copyright (c) 2015 Lemura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
